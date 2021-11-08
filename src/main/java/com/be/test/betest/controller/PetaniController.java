332076ca90b3f0f9a2369c89f8e4097fc7643590
bimosyahputro <bimosyahputro88@gmail.com>
2021-11-08 20:56:46 +0700

    init petani controller

--- src/main/java/com/be/test/betest/controller/PetaniController.java
+++ src/main/java/com/be/test/betest/controller/PetaniController.java
@@ -1,4 +1,16 @@
 package com.be.test.betest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/petani")
 public class PetaniController {
+
+    @GetMapping(value = "checkAPI")
+    public String checkApi(){
+        return "Hallo";
+    }
+
 }

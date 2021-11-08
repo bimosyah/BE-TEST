3ce3144c71fd82345e94c651932383e00f319dd2
bimosyahputro <bimosyahputro88@gmail.com>
2021-11-08 21:58:21 +0700

    add func add for new data

--- src/main/java/com/be/test/betest/controller/PetaniController.java
+++ src/main/java/com/be/test/betest/controller/PetaniController.java
@@ -1,16 +1,25 @@
 package com.be.test.betest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.be.test.betest.entities.PetaniEntity;
+import com.be.test.betest.repositories.PetaniRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/petani")
 public class PetaniController {
+
+    @Autowired
+    PetaniRepository petaniRepository;
 
     @GetMapping(value = "checkAPI")
     public String checkApi(){
         return "Hallo";
     }
 
+    @PostMapping(value = "add")
+    public String add(@RequestBody PetaniEntity param){
+        petaniRepository.save(param);
+        return "suksess";
+    }
 }

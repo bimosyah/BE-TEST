7bee89f1b23e90efedea32bccc58a758f6680376
bimosyahputro <bimosyahputro88@gmail.com>
2021-11-08 23:02:13 +0700

    create response generator

---
+++ src/main/java/com/be/test/betest/response/CommonResponseGenerator.java
@@ -0,0 +1,15 @@
+package com.be.test.betest.response;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonResponseGenerator<T>{
+    public <T> CommonResponse<T> successResponse(T datas, String message){
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setStatus("200");
+        commonResponse.setMessage(message);
+        commonResponse.setDatas(datas);
+
+        return commonResponse;
+    }
+}

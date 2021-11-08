10e6d69ecb2ce5b7d80138ddc12510474ea2573d
bimosyahputro <bimosyahputro88@gmail.com>
2021-11-08 21:56:16 +0700

    add petani repository

---
+++ src/main/java/com/be/test/betest/repositories/PetaniRepository.java
@@ -0,0 +1,9 @@
+package com.be.test.betest.repositories;
+
+import com.be.test.betest.entities.PetaniEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetaniRepository extends JpaRepository<PetaniEntity, Long> {
+}

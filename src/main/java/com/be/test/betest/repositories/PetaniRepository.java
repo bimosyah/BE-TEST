602267fe57ff06c7316b07b2b30cb2b7e43420e1
bimosyahputro <bimosyahputro88@gmail.com>
2021-11-09 21:25:40 +0700

    add query for get data name and code

--- src/main/java/com/be/test/betest/repositories/PetaniRepository.java
+++ src/main/java/com/be/test/betest/repositories/PetaniRepository.java
@@ -2,8 +2,18 @@ package com.be.test.betest.repositories;
 
 import com.be.test.betest.entities.PetaniEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PetaniRepository extends JpaRepository<PetaniEntity, Integer> {
+
+    @Query(value = "SELECT * FROM petani WHERE nama LIKE %:search%", nativeQuery = true)
+    List<PetaniEntity> searchByName(@Param("search") String search);
+
+    @Query(value = "SELECT * FROM petani WHERE code LIKE %:search%", nativeQuery = true)
+    List<PetaniEntity> searchByCode(@Param("search") String search);
 }
